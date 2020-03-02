7cafc4f499777a36286e94489f4d0e7544074d9d
shinyay <shinya.com@gmail.com>
2020-03-02 22:11:15 +0900

    Modify cloud stream dependency as using RabbitMQ

--- spring-cloud-stream-demo/build.gradle.kts
+++ spring-cloud-stream-demo/build.gradle.kts
@@ -18,7 +18,7 @@ extra["springCloudVersion"] = "Hoxton.SR1"
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.cloud:spring-cloud-stream")
+	implementation("org.springframework.cloud:spring-cloud-starter-stream-rabbit")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
