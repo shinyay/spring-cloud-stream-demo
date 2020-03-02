9aa6241b83954c0d0dfa4bfb02705aa453768aec
shinyay <shinya.com@gmail.com>
2020-03-02 22:47:42 +0900

    Add Processor Interface

--- spring-cloud-stream-demo/src/main/kotlin/io/pivotal/shinyay/SpringCloudStreamDemoApplication.kt
+++ spring-cloud-stream-demo/src/main/kotlin/io/pivotal/shinyay/SpringCloudStreamDemoApplication.kt
@@ -1,13 +1,20 @@
 package io.pivotal.shinyay
 
+import io.pivotal.shinyay.entity.LogMessage
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.stream.annotation.EnableBinding
+import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.cloud.stream.messaging.Processor
+import org.springframework.messaging.handler.annotation.SendTo
 
 @SpringBootApplication
 @EnableBinding(Processor::class)
-class SpringCloudStreamDemoApplication
+class SpringCloudStreamDemoApplication {
+	@StreamListener(Processor.INPUT)
+	@SendTo(Processor.OUTPUT)
+	fun logMessage(log: LogMessage): LogMessage = LogMessage("[1]: ${log.message}")
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringCloudStreamDemoApplication>(*args)
