64d5e1497ec1216fd52f778794944418da9a1ae0
shinyay <shinya.com@gmail.com>
2020-03-02 22:31:47 +0900

    Add Stream Processor Enabled

--- spring-cloud-stream-demo/src/main/kotlin/io/pivotal/shinyay/SpringCloudStreamDemoApplication.kt
+++ spring-cloud-stream-demo/src/main/kotlin/io/pivotal/shinyay/SpringCloudStreamDemoApplication.kt
@@ -2,8 +2,11 @@ package io.pivotal.shinyay
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.stream.annotation.EnableBinding
+import org.springframework.cloud.stream.messaging.Processor
 
 @SpringBootApplication
+@EnableBinding(Processor::class)
 class SpringCloudStreamDemoApplication
 
 fun main(args: Array<String>) {
