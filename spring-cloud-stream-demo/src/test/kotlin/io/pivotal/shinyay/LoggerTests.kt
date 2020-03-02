eb11302f9af41f0d19f29b720b3667234e186cc7
shinyay <shinya.com@gmail.com>
2020-03-02 23:12:04 +0900

    Add simple message send-and-get test

---
+++ spring-cloud-stream-demo/src/test/kotlin/io/pivotal/shinyay/LoggerTests.kt
@@ -0,0 +1,26 @@
+package io.pivotal.shinyay
+
+import io.pivotal.shinyay.entity.LogMessage
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.cloud.stream.messaging.Processor
+import org.springframework.cloud.stream.test.binder.MessageCollector
+import org.springframework.messaging.support.MessageBuilder
+
+
+class LoggerTests(private val pipe: Processor, private val messageCollector: MessageCollector) {
+
+    @Test
+    @DisplayName("whenSendMessage_thenResponseShouldUpdateText")
+    fun whenSendMessage_thenResponseShouldUpdateText() {
+        pipe.input()
+                .send(
+                        MessageBuilder.withPayload(LogMessage("This my message")).build()
+                )
+        val payload = messageCollector.forChannel(pipe.output())
+                .poll()
+                .payload
+        assertThat(payload.toString()).isEqualTo("Hello!")
+    }
+}
