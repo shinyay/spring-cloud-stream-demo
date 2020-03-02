5c1fccc7196050e67af21919c320858c8d13b35d
shinyay <shinya.com@gmail.com>
2020-03-02 23:31:42 +0900

    Add RabitMQ Binder dependency

--- spring-cloud-stream-demo/build.gradle.kts
+++ spring-cloud-stream-demo/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-starter-stream-rabbit")
+	implementation("org.springframework.cloud:spring-cloud-stream-binder-rabbit")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
